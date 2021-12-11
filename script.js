7553866dd3c407f9047ffa0c6391031310c315e4
Renato Santos <santos.renato@hotmail.com>
2021-12-10 22:24:09 -0300

    calc IMC

---
+++ script.js
@@ -0,0 +1,44 @@
+
+const calcular = document.getElementById('calcular');
+
+
+function imc() {
+    const nome = document.getElementById('nome').value;
+    const altura = document.getElementById('altura').value;
+    const peso = document.getElementById('peso').value;
+    const resultado = document.getElementById('resultado');
+    const container = document.getElementsByClassName('container');
+
+    if (nome !== '' && altura !== '' && peso !== '') {
+
+        const valorIMC = (peso / (altura * altura)).toFixed(2);
+
+        let classificacao = '';
+        
+        if (valorIMC < 17 ) {
+            container.style.background = "red";
+        }else if(valorIMC < 18.5){
+            classificacao = 'abaixo do peso. ';
+            
+        }else if(valorIMC < 25){
+            classificacao = 'com o peso normal';
+        }else if(valorIMC < 30){
+            classificacao = 'um pouco acima do peso';
+        }else if(valorIMC < 35){
+            classificacao = 'com obesidade grau 1';
+        }else if(valorIMC < 40){
+            classificacao = 'com obesidade grau 2';
+        }else{
+            classificacao = 'com obesidade grau 3 (Mórbida)';
+        }
+
+        resultado.textContent = nome + ' o seu imc é ' + valorIMC + ' e você está ' + classificacao;
+
+    } else {
+        resultado.textContent = 'Preencha todos os campos'
+    }
+
+}
+
+calcular.addEventListener('click', imc);
+
